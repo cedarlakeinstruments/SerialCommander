a8bfcfbab88291c7aa6c44cf6b892ddc35e36613
cedarlakeinstruments <lyndon@cedarlakeinstruments.com>
2017-01-07 13:37:15 -0600

    Added code files

---
+++ SerialCommander/RelayControllerForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace SerialCommander
+{
+    partial class RelayControllerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.listBoxPorts = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(125, 55);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 40);
+            this.buttonSend.TabIndex = 2;
+            this.buttonSend.Text = "Set";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.labelStatus.Location = new System.Drawing.Point(0, 172);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(36, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(62, 17);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Relay 1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(36, 66);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Relay 2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(36, 89);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(62, 17);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Relay 3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(36, 112);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(62, 17);
+            this.checkBox4.TabIndex = 7;
+            this.checkBox4.Text = "Relay 4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // listBoxPorts
+            // 
+            this.listBoxPorts.FormattingEnabled = true;
+            this.listBoxPorts.Location = new System.Drawing.Point(2, 2);
+            this.listBoxPorts.Name = "listBoxPorts";
+            this.listBoxPorts.Size = new System.Drawing.Size(120, 17);
+            this.listBoxPorts.TabIndex = 8;
+            this.listBoxPorts.SelectedIndexChanged += new System.EventHandler(this.listBoxPorts_SelectedIndexChanged);
+            // 
+            // RelayControllerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 185);
+            this.Controls.Add(this.listBoxPorts);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "RelayControllerForm";
+            this.Text = "Serial Relay Controller 1.0";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.ListBox listBoxPorts;
+    }
+}
+
---
+++ SerialCommander/RelayControllerForm.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace SerialCommander
+{
+    /// <summary>
+    /// Controller for DC 5V 12V 2-Channel RS232 Serial Control Relay Switch Board SCM PC SController
+    /// </summary>
+    public partial class RelayControllerForm : Form
+    {
+        SerialPort _port;
+        public RelayControllerForm()
+        {
+            InitializeComponent();
+            string[] ports = SerialPort.GetPortNames();
+            if (ports != null)
+            {
+                listBoxPorts.Items.AddRange(ports);
+            }
+            else
+            {
+                MessageBox.Show("No serial ports found");
+                buttonSend.Enabled = false;
+            }
+        }
+
+        /// <summary>
+        /// Open serial port
+        /// </summary>
+        /// <param name="portName">COMn</param>
+        /// <returns>true if connected successfully</returns>
+        bool connect(string portName)
+        {
+            bool connected = false;
+            try
+            {
+                if (_port != null && _port.IsOpen)
+                {
+                    _port.Close();
+                }
+                _port = new SerialPort(portName, 9600);
+                _port.Handshake = Handshake.None;
+                _port.DataBits = 8;
+                _port.StopBits = StopBits.One;
+                _port.Parity = Parity.None;
+                _port.Open();
+                connected = true;
+                Thread.Sleep(10);
+            }
+            catch (System.IO.IOException )
+            {
+            }
+            return connected;
+        }
+
+        /// <summary>
+        /// Handles the relay control button
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            int ch1 = checkBox1.Checked ? 1 : 0;
+            int ch2 = checkBox2.Checked ? 2 : 0;
+            int ch3 = checkBox3.Checked ? 4 : 0;
+            int ch4 = checkBox4.Checked ? 8 : 0;
+
+            byte cmd = (byte)(ch1+ch2+ch3+ch4);
+            _port.Write(new byte[] { cmd }, 0, 1);
+        }
+
+        /// <summary>
+        /// Select a COM port
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void listBoxPorts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox lb = sender as ListBox;
+            string portName = (string)lb.SelectedItem;
+            if (portName != null)
+            {
+                if (connect(portName) == true)
+                {
+                    labelStatus.Text = "Connected to " + portName;
+                }
+                else
+                {
+                    labelStatus.Text = "Could not connect to " + portName;
+                }
+            }
+        }
+    }
+}
